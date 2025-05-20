c4bbd27754c9d017e7030d2f65e87647f2079bfb
Danny Tuppeny <danny@tuppeny.com>
2025-05-20 17:52:38 +0100

    Create dev.nix

---
+++ dev.nix
@@ -0,0 +1,24 @@
+{pkgs}: {
+  channel = "stable-24.05";
+  packages = [
+    pkgs.jdk17
+    pkgs.unzip
+  ];
+  idx.extensions = [
+
+  ];
+  idx.previews = {
+    previews = {
+      android = {
+        command = [
+          "flutter"
+          "run"
+          "--machine"
+          "-d"
+          "localhost:5555"
+        ];
+        manager = "flutter";
+      };
+    };
+  };
+}
